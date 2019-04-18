91fd06648a617244e3be562b97490ac0ea656fda
ashleyloomis <ashleymloomis@gmail.com>
2019-04-17 22:32:05 -0500

    Initial Commit

---
+++ HappyHourApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyHourApp
+//
+//  Created by student7 on 4/17/19.
+//  Copyright © 2019 student7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
